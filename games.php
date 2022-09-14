9f4462f3f6125acc3e1550527b137ea7f2e74788
CommerceAdmin <94999106+CommerceAdmin@users.noreply.github.com>
2022-09-14 16:19:30 +0300

    Add files via upload
    games.php

---
+++ games.php
@@ -0,0 +1,309 @@
+<?php
+$games = [
+    [
+        'name' => "Seven's on Fire +",
+        'id' => '1002',
+        'image' => 'images/GamePreviews/SevensOnFire+/Seven`s On Fire_512x512.png'
+    ],
+    [
+        'name' => 'Fire Rage +',
+        'id' => '1003',
+        'image' => 'images/GamePreviews/FireRage+/FireRage_512x512.png'
+    ],
+    [
+        'name' => 'Roll of Ramses',
+        'id' => '1004',
+        'image' => 'images/GamePreviews/RollOfRamses/1.jpg'
+    ],
+    [
+        'name' => 'Rich Fruits',
+        'id' => '1013',
+        'image' => 'images/GamePreviews/RichFruits/RichFruits_pict-512x512.png'
+    ],
+    [
+        'name' => 'Ultra 7 Hot',
+        'id' => '1014',
+        'image' => 'images/GamePreviews/Ultra7Hot/Ultra7Hot_512x512.png'
+    ],
+    [
+        'name' => 'European RouletteS',
+        'id' => '1017',
+        'image' => 'images/GamePreviews/EuropianRoulettes/images.jpg'
+    ],
+    [
+        'name' => 'King of Jewels',
+        'id' => '1018',
+        'image' => 'images/GamePreviews/KingsOfJewels/KingOfJewels_512x512.png'
+    ],
+    [
+        'name' => 'Gates of Avalon',
+        'id' => '1019',
+        'image' => 'images/GamePreviews/GatesOfAvalon/1.jpg'
+    ],
+    [
+        'name' => 'Blackjack',
+        'id' => '1023',
+        'image' => 'images/GamePreviews/BlackJack/BJ_512x512.png'
+    ],
+    [
+        'name' => "Dolphin's Shell",
+        'id' => '1024',
+        'image' => 'images/GamePreviews/DolphinsShell/Dolphin`s Shell_pict-512x512.png'
+    ],
+    [
+        'name' => 'Tropical Fruits',
+        'id' => '1025',
+        'image' => 'images/GamePreviews/TropicalFruit/tropical fruit 512x512.png'
+    ],
+    [
+        'name' => "'Money', 'MON026'",
+        'id' => '1026',
+        'image' => 'images/GamePreviews/Money/Money_512x512.png'
+    ],
+    [
+        'name' => 'Lady Luck',
+        'id' => '1027',
+        'image' => 'images/GamePreviews/LadyLuck/LadyLuck_pict_512x512.png'
+    ],
+    [
+        'name' => 'Pirates Fortune',
+        'id' => '1028',
+        'image' => 'images/GamePreviews/PiratesFortune/Pirates_pict 512x512.png'
+    ],
+    [
+        'name' => 'Golden Scatter',
+        'id' => '1029',
+        'image' => 'images/GamePreviews/GoldenScatter/GS_pict-512x512.png'
+    ],
+    [
+        'name' => 'Russian Poker',
+        'id' => '1030',
+        'image' => 'images/GamePreviews/RussianPoker/Russian-Poker_pict512x512.png'
+    ],
+    [
+        'name' => 'Bananas',
+        'id' => '1031',
+        'image' => 'images/GamePreviews/Bananas/Bananas_pict-512x512.png'
+    ],
+    [
+        'name' => 'Mysterious Jewel',
+        'id' => '1032',
+        'image' => 'images/GamePreviews/MysteriousJewels/MJ_pict-512x512.png'
+    ],
+    [
+        'name' => 'Always Cherry Lotto',
+        'id' => '1033',
+        'image' => 'images/GameLotoPreviews/AlwaysCherry/ALwaysCherry_pict-512x512.png'
+    ],
+    ///////////////////
+    [
+        'name' => 'Hot Slot Lotto',
+        'id' => '1034',
+        'image' => 'images\GameLotoPreviews\HotSlot\HotSlot_pict-512x512.png'
+    ],
+    [
+        'name' => 'Golden Scatter Lotto',
+        'id' => '1035',
+        'image' => 'images\GameLotoPreviews\GoldenScatter\GoldenScatterLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Scatter Wins Lotto',
+        'id' => '1036',
+        'image' => 'images\GameLotoPreviews\ScaterWins\ScatterWins_pict-512x512.png'
+    ],
+    [
+        'name' => 'Golden Harvest Lotto',
+        'id' => '1037',
+        'image' => 'images\GameLotoPreviews\GoldenHarvest\Golden-Harvest_pict-512x512.png'
+    ],
+    [
+        'name' => 'Bananas Lotto',
+        'id' => '1038',
+        'image' => 'images\GameLotoPreviews\Bananas\Bananas_pict-512x512.png'
+    ],
+    [
+        'name' => 'Dolphins Lotto',
+        'id' => '1039',
+        'image' => 'images\GameLotoPreviews\BeautyDolphins\BeautyDolphins_pict-512x512.png'
+    ],
+    [
+        'name' => 'Money Lotto',
+        'id' => '1040',
+        'image' => 'images\GameLotoPreviews\Money\Money_pict-512x512.png'
+    ],
+    [
+        'name' => 'Lucky Lady Glamour Lotto',
+        'id' => '1041',
+        'image' => 'images\GameLotoPreviews\LackyLadysGlamour\LLGlamour_pict-512x512.png'
+    ],
+    [
+        'name' => 'Nautilus Lotto',
+        'id' => '1042',
+        'image' => 'images\GameLotoPreviews\Nautilus\Nautilus_pict-512x512.png'
+    ],
+    [
+        'name' => 'Crazy Barmen Lotto',
+        'id' => '1043',
+        'image' => 'images\GameLotoPreviews\CrazyBarmen\CrazyBarmen_pict-512x512.png'
+    ],
+    [
+        'name' => 'Gates Of Avalon Lotto',
+        'id' => '1044',
+        'image' => 'images\GameLotoPreviews\GatesOfAvalon\AvalonLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Hot Sevens Lotto',
+        'id' => '1045',
+        'image' => 'images\GameLotoPreviews\HotSevens\HotSevensLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Hit Jewels Lotto',
+        'id' => '1046',
+        'image' => 'images\GameLotoPreviews\HitJewels\HitJewels_pict-512x512.png'
+    ],
+    [
+        'name' => 'King Of Jewels Lotto',
+        'id' => '1047',
+        'image' => 'images\GameLotoPreviews\KingsOfJewels\KingOfJewelsLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Book Of Winners Lotto',
+        'id' => '1048',
+        'image' => 'images\GameLotoPreviews\BookOfWinners\Book-of-WinnersLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'ComputerWorld Lotto',
+        'id' => '1049',
+        'image' => 'images\GameLotoPreviews\ComputerWorld\ComputerWorldLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Triple Diamond Lotto',
+        'id' => '1050',
+        'image' => 'images\GameLotoPreviews\TripleDiamond\TripleDiamond_pict-512x512.png'
+    ],
+    [
+        'name' => 'Mariner Lotto',
+        'id' => '1051',
+        'image' => 'images\GameLotoPreviews\Mariner\MarinerLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Hearts Lotto',
+        'id' => '1052',
+        'image' => 'images\GameLotoPreviews\Hearts\HeartsLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Captain Lotto',
+        'id' => '1053',
+        'image' => 'images\GameLotoPreviews\Captain\CaptainLoto_pict-512x512.png'
+    ],
+    [
+        'name' => 'Simple Diamond',
+        'id' => '1054',
+        'image' => 'images\GamePreviews\SimpleDiamond\SimpleDiamond_pict-512x512.png'
+    ],
+    [
+        'name' => 'Book Of Sphinx',
+        'id' => '1055',
+        'image' => 'images\GamePreviews\MagicBookOfSphinx\BofS_pict-512x512_vers.png'
+    ],
+    [
+        'name' => 'Extra Super 7',
+        'id' => '1056',
+        'image' => 'images\GamePreviews\ExtraSuper7\ExS7_512x512.png'
+    ],
+    [
+        'name' => 'Fortune Star',
+        'id' => '1057',
+        'image' => 'images\GamePreviews\Fortune Star.jpg'
+    ],
+    [
+        'name' => 'Box of Ra',
+        'id' => '1058',
+        'image' => 'images\GamePreviews\Box of Ra.jpg'
+    ],
+    [
+        'name' => 'Poker 7 Deuces Wild',
+        'id' => '1059',
+        'image' => 'images\GamePreviews\Poker 7 Deuces Wild.jpg'
+    ],
+    [
+        'name' => 'Poker 7 Joker Wild K+',
+        'id' => '1061',
+        'image' => 'images\GamePreviews\Poker 7 Joker Wild K+.jpg'
+    ],
+    [
+        'name' => 'Poker 7 Bonus Deuces Wild',
+        'id' => '1062',
+        'image' => 'images\GamePreviews\Poker 7 Bonus Deuces Wild.jpg'
+    ],
+    [
+        'name' => 'Rise Of Imperium',
+        'id' => '1063',
+        'image' => 'images\GamePreviews\Rise Of Imperium.jpg'
+    ],
+    [
+        'name' => 'European Roulette network',
+        'id' => '1064',
+        'image' => 'images\GamePreviews\European Roulette network.jpg'
+    ],
+    [
+        'name' => 'FORTUNE Wheel',
+        'id' => '1066',
+        'image' => 'images\GamePreviews\FORTUNE Wheel.jpg'
+    ],
+    [
+        'name' => 'FORTUNE Wheel Network',
+        'id' => '1067',
+        'image' => 'images\GamePreviews\FORTUNE Wheel Network.jpg'
+    ],
+    [
+        'name' => 'Golden Harvest',
+        'id' => '1068',
+        'image' => 'images\GameLotoPreviews\GoldenHarvest\Golden-Harvest_pict-512x512.png'
+    ],
+    [
+        'name' => 'Hot Sevens',
+        'id' => '1069',
+        'image' => 'images\GamePreviews\HotSevens.jpg'
+    ],
+    [
+        'name' => 'Crazy Barmen',
+        'id' => '1070',
+        'image' => 'images\GamePreviews\CrazyBarmen.jpg'
+    ],
+    [
+        'name' => 'Nautilus',
+        'id' => '1071',
+        'image' => 'images\GamePreviews\Nautilus.jpg'
+    ],
+    [
+        'name' => 'Mariner',
+        'id' => '1072',
+        'image' => 'images\GamePreviews\Mariner.jpg'
+    ],
+    [
+        'name' => 'Hit Jewels',
+        'id' => '1073',
+        'image' => 'images\GamePreviews\Hit Jewels.jpg'
+    ],
+    [
+        'name' => 'Computer World',
+        'id' => '1074',
+        'image' => 'images\GamePreviews\Computer World.jpg'
+    ],
+    [
+        'name' => "Joker's Fruit Golden Edition",
+        'id' => '1075',
+        'image' => 'images\GamePreviews\Jokers Fruit Golden Edition.jpg'
+    ],
+    [
+        'name' => 'Fruit Party Deluxe',
+        'id' => '1076',
+        'image' => 'images\GamePreviews\Fruit Party Deluxe.jpg'
+    ],
+    [
+        'name' => 'Aztec Century',
+        'id' => '1077',
+        'image' => 'images\GamePreviews\Aztec Century.jpg'
+    ],
+];
